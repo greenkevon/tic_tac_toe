13cda583ad19333ed9bfc703b35a1ae12026c947
Kevon Green <greenkevon@gmail.com>
2014-04-26 19:36:32 -0500

    Determine and display winner. Raise error for specific events, GameOver, SpotTaken and PlayOutOfTurn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,10 +21,10 @@ class GamesController < ApplicationController
       @game.play(session[:player_name], params[:x].to_i, params[:y].to_i, 'O')
     end
 
-
     redirect_to game_path(@game.name)
-  rescue Game::SpotTaken
-    redirect_to game_path(@game.name), error: 'Spot taken'
+
+  rescue Game::SpotTaken, Game::PlayOutOfTurn, Game::GameOver => e
+    redirect_to game_path(@game.name), notice: e.class.to_s.demodulize.titleize
   end
 
   def show
@@ -41,14 +41,18 @@ end
 
 class Game
   SpotTaken = Class.new(StandardError)
+  PlayOutOfTurn = Class.new(StandardError)
+  GameOver = Class.new(StandardError)
+
   FirstPlayVal = 'X'
 
-  attr_reader :players, :name, :data
+  attr_reader :players, :name, :data, :winner, :last_player
 
   def initialize(name)
     @name = name
     @players = []
-    @data = 3.times.map { 3.times.map { '' } }
+    # @data = 3.times.map { 3.times.map { '' } }
+    @data = Array.new(3) { Array.new(3) }
   end
 
   def join(player_name)
@@ -56,13 +60,38 @@ class Game
   end
 
   def play(player_name, x, y, mark)
-    if data[x][y].present?
-      raise SpotTaken
-    else
-      data[x][y] = mark.to_s.upcase #player_name
-    end
+    raise GameOver if find_winner_play
+    raise SpotTaken if data[x][y].present?
+    raise PlayOutOfTurn if @last_player == player_name
+
+    data[x][y] = mark.to_s.upcase #player_name
+    @last_player = player_name
 
     # check if someone has won the game
+    @winner = @last_player if find_winner_play
   end
 
+  private
+
+  def find_winner_play
+    winning_combinations.each do |combination|
+      #move to next array if array only contain nils
+      next if combination.compact.empty?
+
+      unique_tokens = combination.uniq
+
+      return unique_tokens.first if unique_tokens.size == 1
+      # return unique_tokens.size == 1
+    end
+    nil
+  end
+
+  def winning_combinations
+    [
+        data[0], data[1], data[2], # horizontal
+        *3.times.map { |c| [data[0][c], data[1][c], data[2][c]] }, # vertical
+        [data[0][0], data[1][1], data[2][2]], # diagonal left to right
+        [data[0][2], data[1][1], data[2][0]] # diagonal right to left
+    ]
+  end
 end
